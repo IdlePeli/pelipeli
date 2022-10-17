d8bffef9f45b0631b3cec1b5dbd622cc37552ea3
eerosav <eero.sav@gmail.com>
2022-10-17 12:46:55 +0300

    menunäkymät aukeaa

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -1,17 +1,33 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    public Canvas woodCanvas;
-    public Canvas FishCanvas;
-    public Canvas Canvas;
-    public GameObject tile;
+    public GameObject WoodCanvas;
+    public GameObject FishCanvas;
+    public GameObject StoneCanvas;
 
     public void SetCanvas(Biome biome)
     {
-        Debug.Log(biome);
+        
+        GameObject canvas = null;
+        
+        switch (biome.name)
+        {
+            case "Forest":
+                canvas = WoodCanvas;
+                break;
+            case "Ocean":
+                canvas = FishCanvas;
+                break;
+            case "Mountain":
+                canvas = StoneCanvas;
+                break;
+        }
+        canvas.SetActive(true);
     }
 }
