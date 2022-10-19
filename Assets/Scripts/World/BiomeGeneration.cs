caecd886a7c3f9ba4ea7ad26d3f4a236bb2bf0df
MikiS <73443709+RAV64@users.noreply.github.com>
2022-10-19 12:56:30 +0300

    🍫

--- Assets/Scripts/World/BiomeGeneration.cs
+++ Assets/Scripts/World/BiomeGeneration.cs
@@ -11,34 +11,31 @@ public class BiomeGeneration : MonoBehaviour
     public Biome mountain;
     public Biome iceWater;
     public Biome snow;
+
     public Biome deepOcean;
     //lisää biomei ja materiaalei joskus(isoi juttui tulos😎) ᓚᘏᗢ 
-    
+
     //Change in unity to manipulate biomegeneration default value 10 with lower values biomes will be smaller
     public int HeightAdjuster = 10;
     public int TemperatureAdjuster = 10;
     public float TerrainDepth = 1;
-    
-    [SerializeField]
+
     public float a = 0.24f;
     public float b = 0.47f;
-    public float oldA;
-    public float oldB;
-    
+    private float oldA;
+    private float oldB;
+
     public HexManager HM;
-    public bool updateButton;
-    
+
     public Biome Get(int xCoord, int zCoord)
     {
-        //f(xCoord)=((nroot(xCoord-a,3)+b)/(2))
-        
-        
         //generate height for given x and y position
-        float x = Mathf.PerlinNoise((float)xCoord / HeightAdjuster, (float)zCoord / HeightAdjuster);
-        float hexHeight = CubeRoot(x-a) - b;
-        
+        float x = Mathf.PerlinNoise((float) xCoord / HeightAdjuster, (float) zCoord / HeightAdjuster);
+        float hexHeight = CubeRoot(x - a) - b;
+
         //generate temperature for given xCoord and y position
-        float temperature = Mathf.PerlinNoise((float) zCoord / TemperatureAdjuster, (float) xCoord / TemperatureAdjuster);
+        float temperature =
+            Mathf.PerlinNoise((float) zCoord / TemperatureAdjuster, (float) xCoord / TemperatureAdjuster);
 
         //set biome based on height and temperature values
         Biome biome = hexHeight switch
@@ -66,11 +63,12 @@ public class BiomeGeneration : MonoBehaviour
 
     public Vector3 HeightPos(Hex hex)
     {
-        float x = Mathf.PerlinNoise((float)hex.xAxis / HeightAdjuster, (float)hex.zAxis / HeightAdjuster);
-        float hexHeight = CubeRoot(x-a) - b;
+        float x = Mathf.PerlinNoise((float) hex.xAxis / HeightAdjuster, (float) hex.zAxis / HeightAdjuster);
+        float hexHeight = CubeRoot(x - a) - b;
         Vector3 position = hex.transform.position;
-        
-        float temperature = Mathf.PerlinNoise((float) hex.zAxis / TemperatureAdjuster, (float) hex.xAxis / TemperatureAdjuster);
+
+        float temperature = Mathf.PerlinNoise((float) hex.zAxis / TemperatureAdjuster,
+            (float) hex.xAxis / TemperatureAdjuster);
 
         //set biome based on height and temperature values
         Biome biome = hexHeight switch
@@ -91,7 +89,7 @@ public class BiomeGeneration : MonoBehaviour
             }
         };
         hex.SetMaterial(biome.material);
-        
+
         return new Vector3(position.x, hexHeight, position.z);
     }
 
@@ -99,11 +97,9 @@ public class BiomeGeneration : MonoBehaviour
     {
         if (oldA != a || oldB != b)
         {
-            updateButton = false;
-            foreach (Hex hex in HM.GetHexList())
-            {
-                hex.transform.position = HeightPos(hex);
-            }
+            oldA = a;
+            oldB = b;
+            foreach (Hex hex in HM.GetHexList()) hex.transform.position = HeightPos(hex);
         }
     }
 
@@ -131,11 +127,11 @@ public class BiomeGeneration : MonoBehaviour
     {
         return adjHexes.All(adjHex => adjHex.biome.type.Equals("ocean"));
     }
-    
-    private static float CubeRoot(float x) {
+
+    private static float CubeRoot(float x)
+    {
         if (x < 0)
             return -Mathf.Pow(-x, 1f / 3f);
-        else
-            return Mathf.Pow(x, 1f / 3f);
+        return Mathf.Pow(x, 1f / 3f);
     }
 }
