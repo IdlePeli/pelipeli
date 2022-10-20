447dea880197586a1f5873a138e615dd7e8bb524
MikiS <73443709+RAV64@users.noreply.github.com>
2022-10-20 11:55:26 +0300

    Extracted face to own file

--- Assets/Scripts/World/Hex.cs
+++ Assets/Scripts/World/Hex.cs
@@ -4,20 +4,6 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-public struct Face
-{
-    public List<Vector3> Vertices { get; }
-    public List<int> Triangles { get; }
-    public List<Vector2> Uvs { get; }
-
-    public Face(List<Vector3> vertices, List<int> triangles, List<Vector2> uvs)
-    {
-        Vertices = vertices;
-        Triangles = triangles;
-        Uvs = uvs;
-    }
-}
-
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
