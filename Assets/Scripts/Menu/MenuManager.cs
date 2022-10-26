21a5b6684fdd81d972fdf915b36ff77b42892f50
eerosav <eero.sav@gmail.com>
2022-10-26 13:54:14 +0300

    aiempi canvas epäaktivoituu uuden avatessa

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
@@ -10,24 +5,25 @@ public class MenuManager : MonoBehaviour
     public GameObject WoodCanvas;
     public GameObject FishCanvas;
     public GameObject StoneCanvas;
+    public GameObject canvas = null;
 
     public void SetCanvas(Biome biome)
     {
-        
-        GameObject canvas = null;
+        if (canvas != null) canvas.SetActive(false);
         
         switch (biome.name)
-        {
-            case "Forest":
-                canvas = WoodCanvas;
-                break;
-            case "Ocean":
-                canvas = FishCanvas;
-                break;
-            case "Mountain":
-                canvas = StoneCanvas;
-                break;
-        }
+            {
+                case "Forest":
+                    canvas = WoodCanvas;
+                    break;
+                case "Ocean":
+                    canvas = FishCanvas;
+                    break;
+                case "Mountain":
+                    canvas = StoneCanvas;
+                    break;
+            }
+        
         canvas.SetActive(true);
     }
 }
