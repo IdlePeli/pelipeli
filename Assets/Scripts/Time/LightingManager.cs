b48d92a70fb4c82e3c5de8772fee2c64b7092b90
eerosav <eero.sav@gmail.com>
2022-11-03 09:41:35 +0200

    valojuttui🔆

--- Assets/Scripts/Time/LightingManager.cs
+++ Assets/Scripts/Time/LightingManager.cs
@@ -8,24 +8,38 @@ public class LightingManager : MonoBehaviour
     private float lightlevel;
     private float time;
     private float intensity;
+    public float worldTime = 0; // for debugging
+    public float nightLengthAdjuster = -1.5f;
+    public float brightnessCap = 1;
+    private float lightY;
     private void Update()
     {
         time = WorldTime.GetTime();
-        if (time > 12)
+        //time = worldTime;
+        if (time < 12)
         {
-            intensity = 1 - (12 - time) / time;
+            intensity = 1 - (12 - time * -nightLengthAdjuster) / 12;
         }
         else
         {
-            intensity = 1-((12 - time) / 12 * -1);
+            intensity = 1+(12 - time * nightLengthAdjuster + (nightLengthAdjuster * 24)) 
+                / 12 * -1;
         }
+        // https://www.desmos.com/calculator/rhwsjcu1pt
+        // a = nightLengthAdjuster
+        // lower value for shorter nights 
+
+        if (intensity > brightnessCap)
+        {
+            intensity = brightnessCap;
+        } 
 
         directionalLight.intensity = intensity;
+
+        lightY = 50 * intensity + 10;
         
-        lightlevel = WorldTime.GetTime() / 12;
-       
-        directionalLight.intensity = lightlevel;
-        lightRotator.transform.rotation = Quaternion.Euler(0, WorldTime.GetTime()*15, 0);
+        lightRotator.transform.rotation = Quaternion.Euler(0, time*15, 0);
+        directionalLight.transform.localPosition = (new Vector3(60, lightY, 0));
         directionalLight.transform.LookAt(lightRotator.transform.position);
     }
 }
